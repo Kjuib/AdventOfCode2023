4fc3aa2b940f022b04e51664a02c93f81cb3cbc0
Heber Billings <heber.billings@gmail.com>
2023-12-08 22:29:40 -0700

    day09

---
+++ src/day09/Day09.kt
@@ -0,0 +1,83 @@
+package day09
+
+import util.RegexUtil
+import java.io.File
+
+class PartA {
+
+    private fun handleData(data: List<Int>): List<Int> {
+//        println("data: ${ data }")
+
+        val newList = mutableListOf<Int>()
+
+        for (i in 0..<(data.size - 1)) {
+            newList.add(data[i + 1] - data[i])
+        }
+
+        if (newList.all { it == 0}) {
+            newList.add(0)
+        } else {
+            val result = handleData(newList)
+            newList.add(result.last() + newList.last())
+        }
+
+        return newList
+    }
+
+    fun run() {
+//        val lines =  File("./src/${ this.javaClass.packageName }/example.txt").readLines()
+        val lines =  File("./src/${ this.javaClass.packageName }/input.txt").readLines()
+
+        val data = lines.map { RegexUtil.parseInt(it, " ")}
+
+        val results = data.map {
+            val newList = handleData(it)
+            (newList.last() + it.last())
+        }
+
+        val total = results.sum()
+        println("total: ${ total }")
+    }
+}
+
+class PartB {
+
+    private fun handleData(data: List<Int>): List<Int> {
+        val newList = mutableListOf<Int>()
+
+        for (i in 0..<(data.size - 1)) {
+            newList.add(data[i + 1] - data[i])
+        }
+
+        if (newList.all { it == 0}) {
+            newList.add(0, 0)
+        } else {
+            val result = handleData(newList)
+            newList.add(0, newList.first() - result.first())
+        }
+
+        return newList
+    }
+
+    fun run() {
+//        val lines =  File("./src/${ this.javaClass.packageName }/example.txt").readLines()
+        val lines =  File("./src/${ this.javaClass.packageName }/input.txt").readLines()
+
+        val data = lines.map { RegexUtil.parseInt(it, " ")}
+
+        val results = data.map {
+            val newList = handleData(it)
+            val calc = it.first() - newList.first()
+
+            calc
+        }
+
+        val total = results.sum()
+        println("total: ${ total }")
+    }
+}
+
+fun main() {
+//    PartA().run()
+    PartB().run()
+}
